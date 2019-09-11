d31e1df688e3212cc5aba361369980ddec7f3e69
Christopher <christojhg@gmail.com>
2019-09-11 17:00:16 -0500

    Tabla lista

--- resources/views/transaccion/compra/create.blade.php
+++ resources/views/transaccion/compra/create.blade.php
@@ -7,35 +7,35 @@
 @section('value_accion', 'Agregar')
 
 @section('content')
-    <div class="well well-sm">
+            <div class="well well-sm">
                 <div class="row">
                     <div class="col-xs-6">
-                        <input id="client" class="form-control typeahead" type="text" placeholder="Cliente" />
+                        <input id="cliente" class="form-control typeahead" type="text" placeholder="Cliente" />
                     </div>
                     <div class="col-xs-2">
-                        <input class="form-control" type="text" placeholder="Ruc" readonly value="{ruc}" />
+                        <input id="ruc" class="form-control" type="text" placeholder="Ruc" />
                     </div>
                     <div class="col-xs-4">
-                        <input class="form-control" type="text" placeholder="Dirección" readonly value="{address}" />
+                        <input id="direccion" class="form-control" type="text" placeholder="Dirección"  />
                     </div>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-xs-7">
-                    <input id="product" class="form-control" type="text" placeholder="Nombre del producto" />
+                    <input id="producto" class="form-control" type="text" placeholder="Nombre del producto" />
                 </div>
                 <div class="col-xs-2">
-                    <input id="quantity" class="form-control" type="text" placeholder="Cantidad" />
+                    <input id="cantidad" class="form-control" type="text" placeholder="Cantidad" />
                 </div>
                 <div class="col-xs-2">
                     <div class="input-group">
                         <span class="input-group-addon" id="basic-addon1">S/.</span>
-                        <input class="form-control" type="text" placeholder="Precio" value="{price}" readonly />
+                        <input id="precio" class="form-control" type="text" placeholder="Precio"  />
                     </div>
                 </div>
                 <div class="col-xs-1">
-                    <button onclick={__addProductToDetail} class="btn btn-primary form-control" id="btn-agregar">
+                    <button class="btn btn-primary form-control" id="adicionar">
                         <i class="glyphicon glyphicon-plus"></i>
                     </button>
                 </div>
@@ -43,46 +43,60 @@
 
             <hr />
 
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th style="width:40px;"></th>
-                    <th>Producto</th>
-                    <th style="width:100px;">Cantidad</th>
-                    <th style="width:100px;">P.U</th>
-                    <th style="width:100px;">Total</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr each={detail}>
-                    <td>
-                        <button onclick={__removeProductFromDetail} class="btn btn-danger btn-xs btn-block">X</button>
-                    </td>
-                    <td>{name}</td>
-                    <td class="text-right">{quantity}</td>
-                    <td class="text-right">$ {price}</td>
-                    <td class="text-right">$ {total}</td>
-                </tr>
-                </tbody>
-                <tfoot>
-                <tr>
-                    <td colspan="4" class="text-right"><b>IVA</b></td>
-                    <td class="text-right">$ {iva.toFixed(2)}</td>
-                </tr>
-                <tr>
-                    <td colspan="4" class="text-right"><b>Sub Total</b></td>
-                    <td class="text-right">$ {subTotal.toFixed(2)}</td>
-                </tr>
-                <tr>
-                    <td colspan="4" class="text-right"><b>Total</b></td>
-                    <td class="text-right">$ {total.toFixed(2)}</td>
-                </tr>
-                </tfoot>
-            </table>
+            <p>Elementos en la Tabla:
+  <div id="adicionados"></div>
+</p>
+<table id="mytable" class="table table-bordered table-hover ">
+  <tr>
+    <th>Nobmre</th>
+    <th>Apellidos</th>
+    <th>C&eacute;dula</th>
+    <th>Eliminar</th>
 
-            <button>
-                Guardar
-            </button>
+  </tr>
+</table>
+<button>Guardar</button>
+
+
+
+
+
+            <script type="text/javascript">
+            $(document).ready(function() {
+            //obtenemos el valor de los input
+
+            $('#adicionar').click(function() {
+              var nombre = document.getElementById("cliente").value;
+              var apellido = document.getElementById("ruc").value;
+              var cedula = document.getElementById("direccion").value;
+              var i = 1; //contador para asignar id al boton que borrara la fila
+              var fila = '<tr id="row' + i + '"><td>' + nombre + '</td><td>' + apellido + '</td><td>' + cedula + '</td><td><button type="button" name="remove" id="' + i + '" class="btn btn-danger btn_remove">Quitar</button></td></tr>'; //esto seria lo que contendria la fila
+
+              i++;
+
+              $('#mytable tr:first').after(fila);
+                $("#adicionados").text(""); //esta instruccion limpia el div adicioandos para que no se vayan acumulando
+                var nFilas = $("#mytable tr").length;
+                $("#adicionados").append(nFilas - 1);
+                //le resto 1 para no contar la fila del header
+                document.getElementById("apellido").value ="";
+                document.getElementById("cedula").value = "";
+                document.getElementById("nombre").value = "";
+                document.getElementById("nombre").focus();
+              });
+            $(document).on('click', '.btn_remove', function() {
+              var button_id = $(this).attr("id");
+                //cuando da click obtenemos el id del boton
+                $('#row' + button_id + '').remove(); //borra la fila
+                //limpia el para que vuelva a contar las filas de la tabla
+                $("#adicionados").text("");
+                var nFilas = $("#mytable tr").length;
+                $("#adicionados").append(nFilas - 1);
+              });
+            });
+            </script>
+
+
 @endsection
 
 
