29c19f952d698e271ab8a12c2776360f90644ab7
Christopher <christojhg@gmail.com>
2019-09-13 16:51:15 -0500

    vistas -m

--- resources/views/transaccion/compra/create.blade.php
+++ resources/views/transaccion/compra/create.blade.php
@@ -7,9 +7,12 @@
 @section('value_accion', 'Agregar')
 
 @section('content')
-
-            <div class="well well-sm">
+            {{-- <div class="well well-sm">
                 <div class="row">
+                    <h1>Provedor</h1>
+                </div>
+                <div class="row">
+
                     <div class="col-xs-6">
                         <input id="cliente" class="form-control typeahead" type="text" placeholder="Cliente" />
                     </div>
@@ -56,16 +59,82 @@
 
               </tr>
             </table>
-            <button>Guardar</button>
-            <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
-                <script src="{{ asset('js/popper.min.js') }}"></script>
-                <script src="{{ asset('js/bootstrap.js') }}"></script>
-                <script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
-                <script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+            <button>Guardar</button> --}}
 
-                <!-- Custom and plugin javascript -->
-                <script src="{{ asset('js/inspinia.js') }}"></script>
-                <script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
+            <div class="col-lg-12">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Agregar</h5>
+                    <div class="ibox-tools">
+                        <a class="collapse-link">
+                            <i class="fa fa-chevron-up"></i>
+                        </a>
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="fa fa-wrench"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-user">
+                            <li><a href="#" class="dropdown-item">Config option 1</a>
+                            </li>
+                            <li><a href="#" class="dropdown-item">Config option 2</a>
+                            </li>
+                        </ul>
+                        <a class="close-link">
+                            <i class="fa fa-times"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="ibox-content">
+                    <form action=""  enctype="multipart/form-data" method="post">
+                        @csrf
+
+                        <div class="form-group  row">
+                            <label class="col-sm-1 col-form-label">Provedor:</label>
+                            <label class="col-form-label">Ruc:</label>
+                            <div class="col-sm-3"><input type="text" class="form-control" name="abrev"></div>
+                             <label class="col-form-label">Nombre:</label>
+                            <div class="col-sm-5"><input type="text" class="form-control" name="abrev"></div>
+                        </div>
+
+                        <div class="form-group row"><label class="col-sm-1 col-form-label">Direccion:</label>
+                             <div class="col-sm-10"><input type="text" class="form-control" name="name"></div>
+                        </div>
+
+                        <div class="form-group  row">
+                            <label class="col-sm-1 col-form-label">Fecha de entrega:</label>
+                             <div class="col-sm-3"><input type="date" class="form-control" name="direccion"></div>
+                             <label class="col-form-label">Atencion:</label>
+                             <div class="col-sm-3"><input type="text" class="form-control" name="direccion"></div>
+                             <label class="col-form-label">Forma de Pago:</label>
+                             <div class="col-sm-3"><input type="text" class="form-control" name="direccion"></div>
+                        </div>
+
+                        <div class="form-group  row">
+                            <label class="col-sm-1 col-form-label">Descripcion</label>
+                             <div class="col-sm-6"><input type="text" class="form-control" name="direccion"></div>
+                             <label class="col-form-label">Moneda:</label>
+                             <div class="col-sm-2"><input type="text" class="form-control" name="direccion"></div>
+                             <label class="col-form-label">TC:</label>
+                             <div class="col-sm-2"><input type="text" class="form-control" name="direccion"></div>
+                        </div>
+
+
+                        <button class="btn btn-primary" type="submit" name="action">Guardar</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+{{-- //Scripts// --}}
+            <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
+            <script src="{{ asset('js/popper.min.js') }}"></script>
+            <script src="{{ asset('js/bootstrap.js') }}"></script>
+            <script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+            <script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+
+            <!-- Custom and plugin javascript -->
+            <script src="{{ asset('js/inspinia.js') }}"></script>
+            <script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
 
 
 
@@ -107,5 +176,3 @@
 
 
 @endsection
-
-
