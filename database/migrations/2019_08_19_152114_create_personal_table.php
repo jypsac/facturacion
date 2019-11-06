1247972e220524df2db04b02716d282da075f47a
Christopher <christojhg@gmail.com>
2019-11-06 16:21:42 -0500

    migraciones extra

--- database/migrations/2019_08_19_152114_create_personal_table.php
+++ database/migrations/2019_08_19_152114_create_personal_table.php
@@ -32,6 +32,31 @@ class CreatePersonalTable extends Migration
             $table->string('foto')->default('perfil.svg');
             $table->timestamps();
         });
+
+        Schema::create('personal_datos_laborales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('personal_id');
+            $table->foreign('personal_id')->references('id')->on('personal')->onDelete('cascade');
+
+            $table->date('fecha_vinculacion')->nullable();
+            $table->date('fecha_retiro')->nullable();
+            $table->string('forma_pago')->nullable();
+            $table->integer('salario')->nullable();
+            $table->string('categoria_ocupacional')->nullable();
+            $table->string('estado_trabajador')->nullable();
+            $table->string('sede')->nullable();
+            $table->string('turno')->nullable();
+            $table->string('departamento_area')->nullable();
+            $table->string('cargo')->nullable();
+            $table->string('tipo_trabajador')->nullable();
+            $table->string('tipo_contrato')->nullable();
+            $table->string('regimen_pensionario')->nullable();
+            $table->string('afiliacion_salud')->nullable();
+            $table->string('banco_renumeracion')->nullable();
+            $table->integer('numero_cuenta')->nullable();
+            $table->text('notas')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -41,6 +66,8 @@ class CreatePersonalTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('personal_datos_laborales');
         Schema::dropIfExists('personal');
+        
     }
 }
