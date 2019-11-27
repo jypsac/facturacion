aef0ea932ff2d4d4ba9682dc04914afa0b8dadda
Fernando Miranda <luisfernandomirandavaldez@gmail.com>
2019-11-27 15:14:22 -0500

    Vista de Ingreso index

--- resources/views/transaccion/garantias/guia_ingreso/index.blade.php
+++ resources/views/transaccion/garantias/guia_ingreso/index.blade.php
@@ -9,10 +9,14 @@
 
 
 @section('content')
-
 <!-- modal -->
-
-                      
+    <div class="wrapper wrapper-content animated fadeInRight">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ibox ">
+                        
+                        
+                            
                             <div id="modal-form" class="modal fade" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
@@ -36,17 +40,41 @@
                                                         </div>
 
                                                             <button class="btn btn-sm btn-primary float-right m-t-n-xs" type="submit"><strong>Enviar</strong></button>
+                                                    </form>
 
-                                                        </form>
-                                                   </div>
+                                                        </div>
                                                 </div>
 
                                             </div>
                                     </div>
                                     </div>
                                 </div>
+                       
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="ibox ">
+                        <div class="ibox-title">
+                            <h5>Vista Previa</h5>
+                            <div class="ibox-tools">
+                                <a class="collapse-link">
+                                    <i class="fa fa-chevron-up"></i>
+                                </a>
+                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                    <i class="fa fa-wrench"></i>
+                                </a>
+                                <ul class="dropdown-menu dropdown-user">
+                                    <li><a href="#" class="dropdown-item">Config option 1</a>
+                                    </li>
+                                    <li><a href="#" class="dropdown-item">Config option 2</a>
+                                    </li>
+                                </ul>
+                                <a class="close-link">
+                                    <i class="fa fa-times"></i>
+                                </a>
+                            </div>
+                        </div>
                         
-<!--/ modal -->
                         <div class="ibox-content">
                             <div class="table-responsive">
                                 <table class="table table-striped table-bordered table-hover dataTables-example" >
