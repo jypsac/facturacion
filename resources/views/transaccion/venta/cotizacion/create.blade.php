2bece4228a2381231763930b5e49659aa552a473
christopher <christojhg@gmail.com>
2020-03-03 11:44:22 -0500

    prueba

--- resources/views/transaccion/venta/cotizacion/create.blade.php
+++ resources/views/transaccion/venta/cotizacion/create.blade.php
@@ -13,7 +13,7 @@
 		<div class="col-lg-12">
             <div class="ibox">
 				<div class="ibox-title">
-                    <h5>Agregar  </h5>
+                    <h5>Agregar </h5>
                     <div class="ibox-tools">
                         <a class="collapse-link">
                             <i class="fa fa-chevron-up"></i>
