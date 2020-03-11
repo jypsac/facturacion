397dda93130043319345938ffd375a1d9c5afd35
D'angelo <dangelota6@gmail.com>
2020-03-11 16:13:33 -0500

    xD

--- app/Cotizacion.php
+++ app/Cotizacion.php
@@ -16,6 +16,9 @@ class Cotizacion extends Model
     public function forma_pago(){
         return $this->belongsTo(forma_pago::class,'forma_pago_id');
     } 
+    public function personal(){
+        return $this->belongsTo(Personal::class,'personal_id');
+    } 
     
 }
 
--- database/seeds/EmpresaSeeder.php
+++ database/seeds/EmpresaSeeder.php
@@ -13,23 +13,21 @@ class EmpresaSeeder extends Seeder
     {
         DB::table('empresa')->insert([
 			'id' => 1 ,
-			'nombre' => 's&r solution service',
-			'razon_social' => 'S & R SOLUTION SERVICE S.A.C.',
-			'ruc' => '20524840902',
-			'telefono' => '4235390',
-			'movil' => '981145518',
-			'correo' => 'esolis@srsac.com',
+			'nombre' => 'HIDROMAX S.A.C.e',
+			'razon_social' => 'HIDROMAX S.A.C.',
+			'ruc' => '20548110719',
+			'telefono' => '(51-1) 247 7161',
+			'movil' => '946 026 042',
+			'correo' => 'ventas@hidromaxsac.com',
 			'pais' => 'Peru',
 			'region_provincia' => 'Lima',
 			'ciudad' => 'Lima',
-			'calle' => 'Av. Bolivia Nro. 180 Of-306 C.C Wilson Plaza',
-			'codigo_postal' => '15001',
-			'rubro' => ' soluciones de problemas de operatividad informática',
+			'calle' => 'Calle Felix Tello Rojas Mz H Lote 25 Urb Honor y Lealtad de Surco',
+			'codigo_postal' => '50304',
+			'rubro' => ' Venta Partes, Piezas, Accesorios.',
 			'moneda_principal' => 'Soles',
-			'descripcion' => 'S&R Solution Service S.A.C., es una empresa orientada a las soluciones de problemas de operatividad informática, principalmente de temas ligados a Hardware de impresión de distintas marca, siendo una de nuestras principales la marca EPSON..
-
-			Gracias a una organización flexible, al desarrollo de nuevas ventajas competitivas y a la permanente optimización de nuestros servicios, buscaremos adecuarnos a las necesidades y requerimientos de colaboración de cada cliente, desde el inicio de sus necesidades.',
-			'foto' => 'logo.jpg',
+			'descripcion' => 'Nos especializamos ela comercialización de bombas y motores óleo hidráulicos de calidad nueva y de las mejores marca líder en el mercado. Por ese motivo ofrecemos y contamos en stock productos de calidad de marcas internacionales como: Rexroth, Parker, Vickers, Eaton, Denison Hydraulics',
+			'foto' => 'logo.png',
 			'created_at' => date('2019-08-01 00:00:00'),
            	'updated_at' => date('2019-08-01 00:00:00')
 		]);
