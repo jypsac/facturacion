6f6d3fc4593baeb44b7d65014b13ab260d6a56de
Fernando <luisfernandomirandavaldez@gmail.com>
2020-07-01 12:58:39 -0500

    creacion de migraciones y vistas de Boletas

--- database/migrations/2020_07_01_120946_create_boleta_table.php
+++ database/migrations/2020_07_01_120946_create_boleta_table.php
@@ -15,6 +15,25 @@ class CreateBoletaTable extends Migration
     {
         Schema::create('boleta', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('id_cotizador')->nullable();
+            $table->foreign('id_cotizador')->references('id')->on('cotizacion')->onDelete('cascade');
+
+            // Facturador Independiente
+            $table->unsignedBigInteger('cliente_id')->nullable();
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+
+            $table->unsignedBigInteger('moneda_id')->nullable();
+            $table->foreign('moneda_id')->references('id')->on('monedas')->onDelete('cascade');
+
+            $table->unsignedBigInteger('forma_pago_id')->nullable();
+            $table->foreign('forma_pago_id')->references('id')->on('forma_pago')->onDelete('cascade');
+
+            $table->string('fecha_emision')->nullable();
+            $table->string('fecha_vencimiento')->nullable();
+
+            $table->string('estado');
+
             $table->timestamps();
         });
     }
