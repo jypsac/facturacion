be3f9c681e6e2724c222e645afa1754eaab76967
Christopher <christojhg@gmail.com>
2020-09-15 11:26:25 -0500

    validacion de tipo cambio

--- app/Http/Controllers/TipoCambioController.php
+++ app/Http/Controllers/TipoCambioController.php
@@ -40,11 +40,35 @@ class TipoCambioController extends Controller
      */
     public function store(Request $request)
     {
-        $consulta=TipoCambio::where('fecha',Carbon::now()->format('Y-m-d'))->first();
-        if($consulta){
-            return "no puede generar otro tipo de cambio , en el mismo dia";
-        }
+        /*varaibles del Index*/
+        $moneda1=Moneda::where('principal',1)->first();
+        $moneda2=Moneda::where('principal',0)->first();
+        $tipo_cambio=TipoCambio::all();
+        /**/
 
+        $consulta=TipoCambio::where('fecha',Carbon::now()->format('Y-m-d'))->first();/*si se hizo hoy el cambio*/
+        if($consulta){
+            $error= "no puede generar otro tipo de cambio , en el mismo dia";
+            return view('maestro.tipo_cambio.index',compact('tipo_cambio','moneda1','moneda2','error'));
+        }
+        $moneda_principal=Moneda::where('principal',1)->first();
+
+        if ($moneda_principal->id =='1')/*pregunta si esta en Soles(Nacional)*/ {
+            $compra=$request->get('compra');
+            $paralelo=$request->get('paralelo');
+            if ($compra<$paralelo) {
+               $error= 'el tipo de Cambio "Paralelo"('.$paralelo.') debe ser menor al tipo de Cambio "Compra("'.$compra.')';
+               return view('maestro.tipo_cambio.index',compact('tipo_cambio','moneda1','moneda2','error'));
+           }
+       }
+       elseif ($moneda_principal->id =='2')/*pregunta si esta en Dolares(Extranjero)*/  {
+            $compra=$request->get('compra');
+            $paralelo=$request->get('paralelo');
+            if ($compra>$paralelo) {
+            $error= 'el tipo de Cambio "Paralelo"('.$paralelo.') debe ser Mayor al tipo de Cambio "Compra("'.$compra.')';
+            return view('maestro.tipo_cambio.index',compact('tipo_cambio','moneda1','moneda2','error'));
+            }
+    }
         $cambio=new TipoCambio;
         $cambio->compra=$request->get('compra');
         $cambio->venta=$request->get('venta');
@@ -52,9 +76,9 @@ class TipoCambioController extends Controller
         $cambio->fecha=Carbon::now()->format('Y-m-d');
         $cambio->save();
 
-        return redirect()->route('tipo_cambio.index');
+    return redirect()->route('tipo_cambio.index');
 
-    }
+}
 
     /**
      * Display the specified resource.
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
