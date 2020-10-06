17862238480671e3b1600c6a3c8ad17f47595998
Christopher Huaman <christojhg@gmail.com>
2020-10-06 15:21:58 -0500

    seeder modificado

--- database/seeds/KardexEntradaRegistroSeeder.php
+++ database/seeds/KardexEntradaRegistroSeeder.php
@@ -15,53 +15,105 @@ class KardexEntradaRegistroSeeder extends Seeder
             'id' => 1 ,
             'kardex_entrada_id' => 1,
             'producto_id' => 1,
-            'cantidad_inicial' => 200,
-            'precio_nacional' => 11,
-            'precio_extranjero' => 39.09,
-            'cantidad' => 200,
+            'cantidad_inicial' => 100,
+            'precio_nacional' => 10,
+            'precio_extranjero' => 2.77,
+            'cantidad' => 100,
             'estado' => 1,
-            'cambio' => 3.55,
-            'created_at' => date('2019-08-01 00:00:00'),
-           	'updated_at' => date('2019-08-01 00:00:00')
+            'cambio' => 3.61,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
         ]);
-        // DB::table('kardex_entrada_registro')->insert([
-        //     'id' => 2 ,
-        //     'kardex_entrada_id' => 1,
-        //     'producto_id' => 2,
-        //     'cantidad_inicial' => 300,
-        //     'precio_nacional' => 21,
-        //     'precio_extranjero' => 74.62,
-        //     'cantidad' => 300,
-        //     'estado' => 1,
-        //     'cambio' => 3.55,
-        //     'created_at' => date('2019-08-01 00:00:00'),
-        //    	'updated_at' => date('2019-08-01 00:00:00')
-        // ]);
-        // DB::table('kardex_entrada_registro')->insert([
-        //     'id' => 3 ,
-        //     'kardex_entrada_id' => 1,
-        //     'producto_id' => 3,
-        //     'cantidad_inicial' => 20,
-        //     'precio_nacional' => 85,
-        //     'precio_extranjero' => 302.05,
-        //     'cantidad' => 20,
-        //     'estado' => 1,
-        //     'cambio' => 3.55,
-        //     'created_at' => date('2019-08-01 00:00:00'),
-        //    	'updated_at' => date('2019-08-01 00:00:00')
-        // ]);
-        // DB::table('kardex_entrada_registro')->insert([
-        //     'id' => 4 ,
-        //     'kardex_entrada_id' => 2,
-        //     'producto_id' => 2,
-        //     'cantidad_inicial' => 7700,
-        //     'precio_nacional' => 100,
-        //     'precio_extranjero' => 355.35,
-        //     'cantidad' => 7700,
-        //     'estado' => 1,
-        //     'cambio' => 3.55,
-        //     'created_at' => date('2019-08-01 00:00:00'),
-        //    	'updated_at' => date('2019-08-01 00:00:00')
-        // ]);
+        DB::table('kardex_entrada_registro')->insert([
+            'id' => 2 ,
+            'kardex_entrada_id' => 1,
+            'producto_id' => 2,
+            'cantidad_inicial' => 100,
+            'precio_nacional' => 20,
+            'precio_extranjero' => 5.54,
+            'cantidad' => 100,
+            'estado' => 1,
+            'cambio' => 3.61,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
+        DB::table('kardex_entrada_registro')->insert([
+            'id' => 3 ,
+            'kardex_entrada_id' => 2,
+            'producto_id' => 3,
+            'cantidad_inicial' => 100,
+            'precio_nacional' => 30,
+            'precio_extranjero' => 8.31,
+            'cantidad' => 100,
+            'estado' => 1,
+            'cambio' => 3.61,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
+        DB::table('kardex_entrada_registro')->insert([
+            'id' => 4 ,
+            'kardex_entrada_id' => 2,
+            'producto_id' => 2,
+            'cantidad_inicial' => 100,
+            'precio_nacional' => 40,
+            'precio_extranjero' => 11.08,
+            'cantidad' => 100,
+            'estado' => 1,
+            'cambio' => 3.61,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
+        DB::table('kardex_entrada_registro')->insert([
+            'id' => 5 ,
+            'kardex_entrada_id' => 3,
+            'producto_id' => 1,
+            'cantidad_inicial' => 50,
+            'precio_nacional' => 180.50,
+            'precio_extranjero' => 50.00,
+            'cantidad' => 50,
+            'estado' => 1,
+            'cambio' => 3.61,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
+        DB::table('kardex_entrada_registro')->insert([
+            'id' => 6 ,
+            'kardex_entrada_id' => 3,
+            'producto_id' => 2,
+            'cantidad_inicial' => 50,
+            'precio_nacional' => 216.60,
+            'precio_extranjero' => 60.00,
+            'cantidad' => 50,
+            'estado' => 1,
+            'cambio' => 3.61,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
+        DB::table('kardex_entrada_registro')->insert([
+            'id' => 7 ,
+            'kardex_entrada_id' => 3,
+            'producto_id' => 3,
+            'cantidad_inicial' => 50,
+            'precio_nacional' => 252.70,
+            'precio_extranjero' => 70.00,
+            'cantidad' => 50,
+            'estado' => 1,
+            'cambio' => 3.61,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
+        DB::table('kardex_entrada_registro')->insert([
+            'id' => 8 ,
+            'kardex_entrada_id' => 3,
+            'producto_id' => 2,
+            'cantidad_inicial' => 50,
+            'precio_nacional' => 288.80,
+            'precio_extranjero' => 80.00,
+            'cantidad' => 50,
+            'estado' => 1,
+            'cambio' => 3.61,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
     }
 }
--- database/seeds/KardexEntradaSeeder.php
+++ database/seeds/KardexEntradaSeeder.php
@@ -19,25 +19,44 @@ class KardexEntradaSeeder extends Seeder
             'almacen_id' => 1,
             'moneda_id' => 1,
             'guia_remision' => "00001",
-            'factura' => "F-0000001",
+            'factura' => "F001-0000001",
             'informacion' => "informacion de prueba 1",
             'user_id' => 1,
-            'created_at' => date('2019-08-01 00:00:00'),
-           	'updated_at' => date('2019-08-01 00:00:00')
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
         ]);
 
-  //       DB::table('kardex_entrada')->insert([
-  //           'id' => 2 ,
-  //           'motivo_id' => 1,
-  //           'provedor_id' => 1,
-  //           'categoria_id' => 1,
-  //           'almacen_id' => 2,
-  //           'moneda_id' => 2,
-  //           'guia_remision' => "00002",
-  //           'factura' => "1",
-  //           'informacion' => "informacion de prueba 2",
-  //           'created_at' => date('2019-08-01 00:00:00'),
-  //          	'updated_at' => date('2019-08-01 00:00:00')
-		// ]);
+        DB::table('kardex_entrada')->insert([
+            'id' => 2 ,
+            'motivo_id' => 1,
+            'provedor_id' => 1,
+            'categoria_id' => 1,
+            'almacen_id' => 2,
+            'moneda_id' => 1,
+            'guia_remision' => "00002",
+            'factura' => "F001-0000002",
+            'informacion' => "informacion de prueba 2",
+            'user_id' => 1,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
+        
+        DB::table('kardex_entrada')->insert([
+            'id' => 3 ,
+            'motivo_id' => 1,
+            'provedor_id' => 1,
+            'categoria_id' => 1,
+            'almacen_id' => 2,
+            'moneda_id' => 2,
+            'guia_remision' => "00003",
+            'factura' => "F001-0000003",
+            'informacion' => "informacion de prueba 3",
+            'user_id' => 1,
+            'created_at' => date('2020-08-01 00:00:00'),
+           	'updated_at' => date('2020-08-01 00:00:00')
+        ]);
+        
+        
+        
     }
 }
