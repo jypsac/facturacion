544c1ef9109cc14ebd57a082dac054b790221e04
Christopher <christojhg@gmail.com>
2020-10-22 16:35:56 -0500

    vehiculos

--- resources/views/planilla/vehiculo/index.blade.php
+++ resources/views/planilla/vehiculo/index.blade.php
@@ -30,6 +30,9 @@
                             <div>
                                 <div class="panel-body" >
                                     <div class="form-group  row">
+                                        <div class="col-sm-12"><img src="{{asset('img/logos/camion.svg')}}" width="100px"></div>
+                                    </div>
+                                    <div class="form-group  row">
                                         <label class="col-sm-2 col-form-label">Placa:</label>
                                         <div class="col-sm-4"><input type="text" class="form-control" name="placa" placeholder="AT4-234"></div>
                                         <label class="col-sm-2 col-form-label">Marca:</label>
@@ -43,18 +46,18 @@
 
                                         <label class="col-sm-2 col-form-label">Año:</label>
                                         <div class="col-sm-4"><input type="text" class="form-control" name="año" placeholder="2020"></div>
-                                   </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
 
-                    </fieldset>
-                    <button class="btn btn-primary" type="submit">Grabar</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </form>
+                        </fieldset>
+                        <button class="btn btn-primary" type="submit">Grabar</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 <!-- / Modal Create  -->
 <div class="wrapper wrapper-content animated fadeInRight">
@@ -103,17 +106,92 @@
                                  <td>{{$vehiculos->marca}}</td>
                                  <td>{{$vehiculos->modelo}}</td>
                                  <td>{{$vehiculos->año}}</td>
-                                 <td>{{$vehiculos->estado_activo}}</td>
-                                 <td><button class="btn btn-info">Editar</button> </td>
-                             </tr>
-                             @endforeach
-                         </tbody>
-                     </table>
-                 </div>
-             </div>
-         </div>
-     </div>
- </div>
+                                 <td>
+                                    @if($vehiculos->estado_activo==0)Activo
+                                    @elseif($vehiculos->estado_activo==1)Desactivado
+                                    @endif
+                                 </td>
+                                 <td><a href="#exampleModal{{$vehiculos->id}}" data-toggle="modal" class="btn btn-info">Editar</a>
+
+                                    <!-- Modal Create  -->
+
+                                    <div class="modal fade" id="exampleModal{{$vehiculos->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel"> Vehiculo</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div style="padding-left: 15px;padding-right: 15px;">
+                                                    {{-- ccccccccccccccccc --}}
+                                                    <div class="ibox-content" style="padding-left: 0px;padding-right: 0px;" align="center">
+
+                                                       <form action="{{route('vehiculo.update',$vehiculos->id)}}"  enctype="multipart/form-data" method="post">
+                                                        @csrf
+                                                        @method('PATCH')
+                                                        <fieldset >
+                                                            <legend> Agregar Vehiculo </legend>
+                                                            <div>
+                                                                <div class="panel-body" >
+                                                                    <div class="form-group  row">
+                                                                        <div class="col-sm-12"><img src="{{asset('img/logos/camion.svg')}}" width="100px"></div>
+                                                                    </div>
+                                                                    <div class="form-group  row">
+                                                                        <label class="col-sm-2 col-form-label">Placa:</label>
+                                                                        <div class="col-sm-4"><input type="text" class="form-control" name="placa" value="{{$vehiculos->placa}}" placeholder="AT4-234"></div>
+                                                                        <label class="col-sm-2 col-form-label">Marca:</label>
+                                                                        <div class="col-sm-4"><input type="text" class="form-control" name="marca" value="{{$vehiculos->marca}}" placeholder="Toyota"></div>
+                                                                    </div>
+
+                                                                    <div class="form-group  row">
+                                                                        <label class="col-sm-2 col-form-label">Modelo:</label>
+                                                                        <div class="col-sm-4"><input type="text" class="form-control" name="modelo" value="{{$vehiculos->modelo}}" placeholder="RAV4"></div>
+
+                                                                        <label class="col-sm-2 col-form-label">Año:</label>
+                                                                        <div class="col-sm-4"><input type="text" class="form-control" name="año" value="{{$vehiculos->año}}" placeholder="2020"></div>
+                                                                    </div>
+                                                                    <div class="form-group  row">
+                                                                         <div class="col-sm-6" align="right">
+                                                                      <label class="col-form-label">Activo/desactivo:</label></div>
+                                                                      <div class="col-sm-6" align="left">
+                                                                        @if($vehiculos->estado_activo == 0)
+                                                                        <div class="switch-button">
+                                                                            <input type="checkbox" name="estado_activo" id="switch-label{{$vehiculos->id}}" class="switch-button__checkbox" checked="">
+                                                                            <label for="switch-label{{$vehiculos->id}}" class="switch-button__label"></label>
+                                                                        </div>
+                                                                        @else
+                                                                        <div class="switch-button">
+                                                                            <input type="checkbox" name="estado_activo" id="aswitch-label{{$vehiculos->id}}" class="switch-button__checkbox" >
+                                                                            <label for="aswitch-label{{$vehiculos->id}}" class="switch-button__label"></label>
+                                                                        </div>
+                                                                        @endif
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                    </fieldset>
+                                                    <button class="btn btn-primary" type="submit">Grabar</button>
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- / Modal Create  -->
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+</div>
 
 </div>
 
@@ -133,6 +211,43 @@
 <script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
 <style>
     .form-control{border-radius: 5px}
+    .col-sm-4{padding-bottom: 10px}
+    :root {
+        --color-button: #fdffff;
+    }
+    .switch-button {
+        display: inline-block;
+        padding-top: 9px;
+        padding-right: 30px;
+    }
+    .switch-button .switch-button__checkbox {
+        display: none;
+    }
+    .switch-button .switch-button__label {
+        background-color:#1f1f1f66;
+        width: 2rem;
+        height: 1rem;
+        border-radius: 3rem;
+        display: inline-block;
+        position: relative;
+    }
+    .switch-button .switch-button__label:before {
+        transition: .6s;
+        display: block;
+        position: absolute;
+        width: 1rem;
+        height: 1rem;
+        background-color: var(--color-button);
+        content: '';
+        border-radius: 50%;
+        box-shadow: inset 0px 0px 0px 1px black;
+    }
+    .switch-button .switch-button__checkbox:checked + .switch-button__label {
+        background-color: #1c84c6;
+    }
+    .switch-button .switch-button__checkbox:checked + .switch-button__label:before {
+        transform: translateX(1rem);
+    }
 </style>
 <!-- Page-Level Scripts -->
 <script>
