039934ae4fca061fe9a294cb1bad7eebdfea57fa
unknown <danielrberru@gmail.com>
2021-02-10 20:54:17 -0500

    marcas buscador

--- resources/views/configuracion_general/marca/index.blade.php
+++ resources/views/configuracion_general/marca/index.blade.php
@@ -283,7 +283,8 @@
 <script src="{{ asset('js/inspinia.js') }}"></script>
 <script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
 
-
+<script src="{{ asset('js/plugins/dataTables/datatables.min.js') }}"></script>
+<script src="{{ asset('js/plugins/dataTables/dataTables.bootstrap4.min.js') }}"></script>
 <script>
 
     $(document).ready(function () {
@@ -296,5 +297,33 @@
     });
 
 </script>
+<script>
+    $(document).ready(function(){
+        $('.dataTables-example').DataTable({
+            pageLength: 25,
+            responsive: true,
+            dom: '<"html5buttons"B>lTfgitp',
+            buttons: [
+            { extend: 'copy'},
+            {extend: 'csv'},
+            {extend: 'excel', title: 'ExampleFile'},
+            {extend: 'pdf', title: 'ExampleFile'},
 
+            {extend: 'print',
+            customize: function (win){
+                $(win.document.body).addClass('white-bg');
+                $(win.document.body).css('font-size', '10px');
+
+                $(win.document.body).find('table')
+                .addClass('compact')
+                .css('font-size', 'inherit');
+            }
+        }
+        ]
+
+    });
+
+    });
+
+</script>
 @endsection
