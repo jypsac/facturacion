198d2664877ca684364be8be8a0aa65d05a96b5d
Christopher <christojhg@gmail.com>
2021-02-24 17:09:10 -0500

    cliente vistas, show

--- resources/views/auxiliar/cliente/show.blade.php
+++ resources/views/auxiliar/cliente/show.blade.php
@@ -9,217 +9,155 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 @section('content')
-
-<div style="padding-top: 20px;padding-bottom: 50px">
-  <div class="container" style=" padding-top: 30px; background: white;">
-    <div class="jumbotron" style="height: 50px;padding:10px">
-     <center><h1>{{$cliente_show->nombre}}</h1> </center>
-   </div>
-
-   <i  class="fa fa-user-o" aria-hidden="true" style="font-size: 35px"></i>&nbsp;&nbsp;
-   <a  class="btn btn-success" href="{{ route('cliente.edit', $cliente_show->id) }}" role="button"> <i class="fa fa-edit"></i></a>
-   <br><br>
-   <div class="row marketing">
-
-    <div class="col-lg-12">
-
-      <div class="row">
-        <div class="col-lg-4">
-          <h4>Nombre:</h4>
-          <p>{{$cliente_show->nombre}}</p>
-        </div>
-        <div class="col-lg-4">
-          <h4>Direccion:</h4>
-          <p>{{$cliente_show->direccion}}</p>
-        </div>
-        <div class="col-lg-4">
-          <h4>Email:</h4>
-          <p>{{$cliente_show->email}}</p>
-        </div>
-      </div><hr>
-
-      <div class="row">
-        <div class="col-lg-4">
-          <h4>Pais:</h4>
-          <p>{{$cliente_show->pais}}</p>
-        </div>
-        <div class="col-lg-4">
-          <h4>Departamento:</h4>
-          <p>{{$cliente_show->departamento}}</p>
-        </div>
-        <div class="col-lg-4">
-          <h4>Ciudad:</h4>
-          <p>{{$cliente_show->ciudad}}</p>
-        </div>
-      </div><hr>
-
-      <div class="row">
-        <div class="col-lg-4">
-          <h4>Codigo Postal:</h4>
-          <p>{{$cliente_show->cod_postal}}</p>
-        </div>
-        <div class="col-lg-4">
-         <h4>Telefono:</h4>
-         <p>{{$cliente_show->numero_documento}}</p>
-       </div>
-       <div class="col-lg-4">
-        <h4>Celular:</h4>
-        <p>{{$cliente_show->celular}}</p>
-      </div>
-
-    </div><hr>
-
-    <div class="row">
-      <div class="col-lg-4">
-        <h4>Documento De Identifiacion:</h4>
-        <p>{{$cliente_show->documento_identificacion}}</p>
-      </div>
-      <div class="col-lg-4">
-       <h4>Numero Documento:</h4>
-       <p>{{$cliente_show->numero_documento}}</p>
-     </div>
-     <div class="col-lg-4">
-      <h4>Tipo de Cliente:</h4>
-      <p>{{$cliente_show->tipo_cliente}}</p>
-    </div>
-
-  </div><hr>
-
+<div class="wrapper wrapper-content  animated fadeInRight">
   <div class="row">
-    <div class="col-lg-4">
-      <h4>Fecha de Aniversario:</h4>
-      <p>{{$cliente_show->aniversario}}</p>
-    </div>
-    <div class="col-lg-4">
-     <h4>Fecha de Registro:</h4>
-     <p>{{$cliente_show->fecha_registro}}</p>
-   </div>
-            <!-- <div class="col-lg-4">
-              <h4>Tipo de Cliente:</h4>
-          <p>{{$cliente_show->tipo_cliente}}</p>
-        </div> -->
-
-      </div><hr>
-
-
-    </div>
-
-  </div>
-
-  {{--Contacto  --}}
-  <!-- Button trigger modal -->
-  <i  style="font-size: 35px" class="fa fa-address-book-o" aria-hidden="true"></i>
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"> Agregar Contacto </button><br>
-
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document" style="margin-left: 490px;">
-      <div class="modal-content" style="width: 702px;">
-        <div class="modal-body" style="padding-top: 0px;">
-          <div>
-            <div >
-              <form action="{{ route('contacto.store',$cliente_show->id) }}"  enctype="multipart/form-data" method="post">
-               @csrf
-               <div >
-                <h1><i class="fa fa-address-book-o" aria-hidden="true"></i>  </h1>
-                <div class="row marketing">
-                  <div class="col-lg-6">
-                    <h4>Nombre del Contacto:</h4>
-                   <input class="form-control" type="text" name="nombre" >
-                    <h4>Cargo:</h4>
-                    <p><input class="form-control " type="text" name="cargo" >
+    <div class="col-sm-8">
+      <div class="ibox">
+        <div class="ibox-content">
+          <h2>Contacto Cliente</h2>
+          <p>Todos los clientes deben ser verificados antes de poder enviar un correo electrónico y configurar un proyecto.</p>
+          <div class="input-group">
+            <input type="text" placeholder="Buscar " class="input form-control">
+            <span class="input-group-append">
+              <button type="button" class="btn btn btn-primary"> <i class="fa fa-search"></i> Buscar</button>
+            </span>
+          </div>
+          <div class="clients-list">
+            <span class="float-right small text-muted">1406 Elementos</span>
+            <ul class="nav nav-tabs">
+              <li><a class="nav-link active" data-toggle="tab" href="#tab-1"><i class="fa fa-user"></i> Contacts</a></li>
+              <li><a class="nav-link" data-toggle="tab" href="#tab-2"><i class="fa fa-plus"></i> Agregar Contacto</a></li>
+            </ul>
+            <div class="tab-content">
+              <div id="tab-1" class="tab-pane active">
+                <div class="full-height-scroll">
+                  <div class="table-responsive">
+                    <table class="table table-striped table-hover" style="font-size: 13px">
+                      <tbody>
+                        @foreach($contacto_show as $contacto)
+                        <tr>
+                          <td class="client-avatar"><img src="https://www.flaticon.es/premium-icon/icons/svg/3772/3772240.svg"> </td>
+                          <td><a href="#contact-1" class="client-link">{{$contacto->nombre}}</a></td>
+                          <td>{{$contacto->cargo}}</td>
+                          <td class="contact-type"><i class="fa fa-envelope"> </i></td>
+                          <td> {{$contacto->email}}</td>
+                          <td class="contact-type"><i class="fa fa-phone"> </i></td>
+                          <td>{{$contacto->telefono}} </td>
+                          <td class="contact-type"><i class="fa fa-phone"> </i></td>
+                          <td>{{$contacto->celular}}</td>
+                          <td class="client-status"><span class="label label-primary">Activo</span></td>
+                          {{-- <td class="client-status"><span class="label label-warning">desactivo</span></td> --}}
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
                   </div>
-                  <div class="col-lg-6">
-                    <h4>Telefono/Celular:</h4>
-                    <div class="row" style="padding-left: 15px">
-                      <input class="form-control col-sm-5" name="telefono" type="text" placeholder="Telefono"> &nbsp; -  &nbsp;<input class="form-control col-sm-5" name="celular" type="text" placeholder="Celular" >
+                </div>
+              </div>
+              <div id="tab-2" class="tab-pane">
+                <div class="full-height-scroll">
+                  <div>
+                   <form action="{{ route('contacto.store',$cliente_show->id) }}"  enctype="multipart/form-data" method="post">
+                     @csrf
+                     <div style="padding-top: 20px">
+                      <div class="row marketing">
+                        <div class="col-lg-6">
+                          <h4>Nombre del Contacto:</h4>
+                          <input class="form-control" type="text" name="nombre" >
+                          <h4>Cargo:</h4>
+                          <p><input class="form-control " type="text" name="cargo" >
+                          </div>
+                          <div class="col-lg-6">
+                            <h4>Telefono/Celular:</h4>
+                            <div class="row" style="padding-left: 15px">
+                              <input class="form-control col-sm-5" name="telefono" type="text" placeholder="Telefono"> &nbsp; -  &nbsp;<input class="form-control col-sm-5" name="celular" type="text" placeholder="Celular" >
+                            </div>
+                            <h4>Email:</h4>
+                            <input class="form-control" name="email" type="text" >
+                            <input type="hidden" name="clientes_id" value="{{$cliente_show->id}}">
+
+                          </div>
+                          <div class="col-lg-6">
+                            <p><input class="btn btn-primary" type="submit" value="Grabar"></p>
+                          </div>
+
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="ibox selected">
+        <div class="ibox-content">
+          <div class="tab-content">
+            <div id="contact-1" class="tab-pane active">
+              <div class="row m-b-lg">
+                <div class="col-lg-12 text-center">
+                  <h2>{{$cliente_show->nombre}}</h2>
+                </div>
+                <div class="col-lg-12">
+                  <button type="button" class="btn btn-primary btn-sm btn-block"><i
+                    class="fa fa-edit"></i> Editar
+                  </button>
+                </div>
+              </div>
+              <div class="client-detail">
+                <div class="full-height-scroll">
+
+                    <table class="table table-striped table-hover" style="font-size: 13px">
+                      <tbody>
+                        <tr>
+                          <td class="client-avatar"><img src="https://www.flaticon.es/premium-icon/icons/svg/3772/3772240.svg"> </td>
+                          <td><a href="#contact-1" class="client-link">{{$contacto->nombre}}</a></td>
+                          <td>a</td>
+                          <td class="contact-type"><i class="fa fa-envelope"> </i></td>
+                          <td> a</td>
+                          <td class="contact-type"><i class="fa fa-phone"> </i></td>
+                          <td>a</td>
+                          <td class="contact-type"><i class="fa fa-phone"> </i></td>
+                          <td>a</td>
+                          <td class="client-status"><span class="label label-primary">Activo</span></td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+
+                <hr/>
+                <strong>Timeline activity</strong>
+                <div id="vertical-timeline" class="vertical-container dark-timeline">
+
+                  <div class="vertical-timeline-block">
+                    <div class="vertical-timeline-icon gray-bg">
+                      <i class="fa fa-bolt"></i>
                     </div>
-                    <h4>Email:</h4>
-                   <input class="form-control" name="email" type="text" >
-                    <input type="hidden" name="clientes_id" value="{{$cliente_show->id}}">
-
-                  </div>
-                  <div class="col-lg-6">
-                    <p><input class="btn btn-primary" type="submit" value="Grabar"></p>
+                    <div class="vertical-timeline-content">
+                      <p>There are many variations of passages of Lorem Ipsum available.
+                      </p>
+                      <span class="vertical-date small text-muted"> 06:10 pm - 11.03.2014 </span>
+                    </div>
                   </div>
 
                 </div>
               </div>
-            </form>
+            </div>
           </div>
-        </div>
 
+
+        </div>
       </div>
     </div>
   </div>
 </div>
-<!-- / Modal -->
-
-<div style="padding-bottom: 10px; padding-top: 5px">
-  @foreach($contacto_show as $contacto)
-  <table>
-
-    <tr>
-      <th>
-        <a  class="btn btn-success" href="{{ route('contacto.editar', $contacto->id) }}" role="button"> <i class="fa fa-edit"></i></a>
-      </th>
-      @if($contacto->primer_contacto==1)
-      @else
-      <th>
-        <a data-toggle="modal" class="btn btn-s-m btn-info" href="#modal-form"><i class="fa fa-star"></i></a>
-      </th>
-      @endif
-    </tr>
-  </table>
-
-  <div class="row marketing">
-   <div class="col-lg-6">
-     <h4>Nombre del Contacto:</h4>
-     <p>{{$contacto->nombre}}</p><hr>
-     <h4>Cargo:</h4>
-     <p>{{$contacto->cargo}}</p><hr>
-   </div>
-   <div class="col-lg-6">
-     <h4>Telefono/Celular:</h4>
-     <p>{{$contacto->telefono}} / {{$contacto->celular}}</p><hr>
-     <h4>Email:</h4>
-     <p>{{$contacto->email}}</p><hr>
-   </div>
-
-
- </div>
- @endforeach
 </div>
-
-</div>
-</div>
-{{-- Modal Contacto Principal --}}
-
-<div id="modal-form" class="modal fade" aria-hidden="true">
-  <div class="modal-dialog" style="margin-top: 12%">
-    <div class="modal-content">
-      <div class="modal-body">
-
-       <div class="ibox-content float-e-margins">
-
-         <h3 class="font-bold col-lg-12" align="center">
-          ¿Esta Seguro que Deseas Cambiar como "Contacto Principal"?
-        </h3>
-        <p align="center">
-         <a class="btn btn-w-m btn-primary" href="">Aceptar</a>
-         {{-- <a class="btn btn-w-m btn-danger"  href="">Cancelar</a> --}}
-         <button type="button" class="btn btn-w-m btn-danger" data-dismiss="modal">Cancelar</button>
-
-       </p>
-     </div>
-
-   </div>
- </div>
-</div>
-</div>
-{{-- Fin Modal Contacto Principal --}}
-
-
+<style>
+  .form-control{border-radius: 5px}
+</style>
 <!-- Mainly scripts -->
 <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
 <script src="{{ asset('js/popper.min.js') }}"></script>
