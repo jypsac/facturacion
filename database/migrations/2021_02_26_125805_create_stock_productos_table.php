7f7cf8da3f7fcf0eb9fba2bdf483237229cfa488
Christopher <christojhg@gmail.com>
2021-02-26 13:07:12 -0500

    creacion de la migracion stock productos

---
+++ database/migrations/2021_02_26_125805_create_stock_productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('producto_id');
+            $table->foreign('producto_id')->references('id')->on('productos')->onDelete('cascade');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_productos');
+    }
+}
