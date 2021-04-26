876a704383d378c8050dfa87cc01f6c46aa9cf32
Christopher Huaman <christojhg@gmail.com>
2021-04-26 16:24:10 -0500

    opciones agregadas al datatable

--- resources/views/inventario/periodo-consulta/index.blade.php
+++ resources/views/inventario/periodo-consulta/index.blade.php
@@ -172,7 +172,25 @@
 			}).done(function(res){
                 var data=JSON.parse(res);
                 $('#tablaid').dataTable({
-                    
+                        pageLength: 25,
+                        responsive: true,
+                        dom: '<"html5buttons"B>lTfgitp',
+                        buttons: [
+                            {extend: 'copy'},
+                            {extend: 'csv'},
+                            {extend: 'excel', title: 'ExampleFile'},
+                            {extend: 'pdf', title: 'ExampleFile'},
+                            {extend: 'print',
+                                customize: function (win){
+                                        $(win.document.body).addClass('white-bg');
+                                        $(win.document.body).css('font-size', '10px');
+
+                                        $(win.document.body).find('table')
+                                                .addClass('compact')
+                                                .css('font-size', 'inherit');
+                                }
+                            }
+                        ],
                     "aaData": data,
                     "columns": [
                         { "data": "producto" },
