72af2f53dd6eb1ade8c33af2dff36b5af5541d62
Chrisopher <christojhg@gmail.com>
2021-10-28 16:25:12 -0500

    mysql importar

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -39,98 +39,101 @@ class EmpresaController extends Controller
     public function store(Request $request)
     {
 
-        $nombre="74894537";
-        $nombre= (string)$nombre;
-    //1- Crear directorio de Laravel
-        //A-Crear archivo Bat para clonar
-        $copy_page = fopen("C:\laragon\www/puntos_bat/".$nombre.".bat", 'a');
-        $texto='robocopy C:\laragon\www\facturacion  C:\laragon\www/'.$nombre.' /e';
-        fwrite($copy_page,$texto);
-        // fclose($copy_page);
+    //     $nombre="74894537";
+    //     $nombre= (string)$nombre;
+    // //1- Crear directorio de Laravel
+    //     //A-Crear archivo Bat para clonar
+    //     $copy_page = fopen("C:\laragon\www/puntos_bat/".$nombre.".bat", 'a');
+    //     $texto='robocopy C:\laragon\www\facturacion  C:\laragon\www/'.$nombre.' /e';
+    //     fwrite($copy_page,$texto);
+    //     // fclose($copy_page);
 
-        //B-Correr el Archivo Bat
-        $c='start /b  C:\laragon\www/puntos_bat/'.$nombre.'.bat';
-        $r=pclose(popen($c, 'r'));
+    //     //B-Correr el Archivo Bat
+    //     $c='start /b  C:\laragon\www/puntos_bat/'.$nombre.'.bat';
+    //     $r=pclose(popen($c, 'r'));
 
-    //2- Crear Base de Datos
-        //A-Crear archivo Bat para crear BD
-        $bdatos = fopen('C:\laragon\www/puntos_bat/bd_'.$nombre.'.bat', 'a');
-        $texto2='cd/
-        cd laragon\bin\mysql\mysql-5.7.24-winx64\bin
-        mysql -u root -e " create DATABASE facturacion_'.$nombre.' ;"';
-        fwrite($bdatos,$texto2);
-        //B-Correr el Archivo Bat
-        $w='start /b  C:\laragon\www/puntos_bat/bd_'.$nombre.'.bat';
-        $r=pclose(popen($w, 'r'));
-        sleep(5);
+    // //2- Crear Base de Datos
+    //     //A-Crear archivo Bat para crear BD
+    //     $bdatos = fopen('C:\laragon\www/puntos_bat/bd_'.$nombre.'.bat', 'a');
+    //     $texto2='cd/
+    //     cd laragon\bin\mysql\mysql-5.7.24-winx64\bin
+    //     mysql -u root -e " create DATABASE facturacion_'.$nombre.' ;"';
+    //     fwrite($bdatos,$texto2);
+    //     //B-Correr el Archivo Bat
+    //     $w='start /b  C:\laragon\www/puntos_bat/bd_'.$nombre.'.bat';
+    //     $r=pclose(popen($w, 'r'));
+    //     sleep(15);
 
-    //3- Borrar .env y Crear .ENV con nueva base de datos
-        //A-Borrar ENV
-        unlink('C:\laragon\www/'.$nombre.'/.env');
-        //B-Crear archivo Bat para crear BD
-        $env = fopen('C:\laragon\www/'.$nombre.'/.env', 'a');
-        $texto_env='APP_NAME=facturacion
-        APP_ENV=local
-        APP_KEY=base64:mJRlTzPKaapP9OqKdqsj7sTQxSa/HwXoGI2q7L6OwKo=
-        APP_DEBUG=true
-        APP_URL=http://localhost
+    // //3- Borrar .env y Crear .ENV con nueva base de datos
+    //     //A-Borrar ENV
+    //     unlink('C:\laragon\www/'.$nombre.'/.env');
+    //     //B-Crear archivo Bat para crear BD
+    //     $env = fopen('C:\laragon\www/'.$nombre.'/.env', 'a');
+    //     $texto_env='
+    //                 APP_NAME=facturacion
+    //                 APP_ENV=local
+    //                 APP_KEY=base64:mJRlTzPKaapP9OqKdqsj7sTQxSa/HwXoGI2q7L6OwKo=
+    //                 APP_DEBUG=true
+    //                 APP_URL=http://localhost
 
-        LOG_CHANNEL=stack
+    //                 LOG_CHANNEL=stack
 
-        DB_CONNECTION=mysql
-        DB_HOST=127.0.0.1
-        DB_PORT=3306
-        DB_DATABASE= facturacion_'.$nombre.'
-        DB_USERNAME=root
-        DB_PASSWORD=
+    //                 DB_CONNECTION=mysql
+    //                 DB_HOST=127.0.0.1
+    //                 DB_PORT=3306
+    //                 DB_DATABASE= facturacion_'.$nombre.'
+    //                 DB_USERNAME=root
+    //                 DB_PASSWORD=
 
-        BROADCAST_DRIVER=log
-        CACHE_DRIVER=file
-        QUEUE_CONNECTION=sync
-        SESSION_DRIVER=file
-        SESSION_LIFETIME=120
+    //                 BROADCAST_DRIVER=log
+    //                 CACHE_DRIVER=file
+    //                 QUEUE_CONNECTION=sync
+    //                 SESSION_DRIVER=file
+    //                 SESSION_LIFETIME=120
 
-        REDIS_HOST=127.0.0.1
-        REDIS_PASSWORD=null
-        REDIS_PORT=6379
+    //                 REDIS_HOST=127.0.0.1
+    //                 REDIS_PASSWORD=null
+    //                 REDIS_PORT=6379
 
-        MAIL_DRIVER=smtp
-        MAIL_HOST=smtp.mailtrap.io
-        MAIL_PORT=2525
-        MAIL_USERNAME=5c6698a7299f48
-        MAIL_PASSWORD=be72971a1aa9f9
-        MAIL_ENCRYPTION=null
+    //                 MAIL_DRIVER=smtp
+    //                 MAIL_HOST=smtp.mailtrap.io
+    //                 MAIL_PORT=2525
+    //                 MAIL_USERNAME=5c6698a7299f48
+    //                 MAIL_PASSWORD=be72971a1aa9f9
+    //                 MAIL_ENCRYPTION=null
 
-        AWS_ACCESS_KEY_ID=
-        AWS_SECRET_ACCESS_KEY=
-        AWS_DEFAULT_REGION=us-east-1
-        AWS_BUCKET=
+    //                 AWS_ACCESS_KEY_ID=
+    //                 AWS_SECRET_ACCESS_KEY=
+    //                 AWS_DEFAULT_REGION=us-east-1
+    //                 AWS_BUCKET=
 
-        PUSHER_APP_ID=
-        PUSHER_APP_KEY=
-        PUSHER_APP_SECRET=
-        PUSHER_APP_CLUSTER=mt1
+    //                 PUSHER_APP_ID=
+    //                 PUSHER_APP_KEY=
+    //                 PUSHER_APP_SECRET=
+    //                 PUSHER_APP_CLUSTER=mt1
 
-        MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"
-        MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"
-        ';
-        fwrite($env,$texto_env);
+    //                 MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"
+    //                 MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"
+    //     ';
 
-    //4- Refresh seedr
-        //A-Crear archivo Bat para crear las migraciones
-        $migrate = fopen('C:\laragon\www/puntos_bat/php_fresh_'.$nombre.'.bat', 'a');
-        $texto_migrate='cd/
-        cd laragon\www/'.$nombre.'
-        php artisan migrate:fresh
-        php artisan migrate:fresh --seed';
-        fwrite($migrate,$texto_migrate);
+    //     fwrite($env,$texto_env);
 
-
+    // //4- Refresh seedr
+    //     //A-Crear archivo Bat para crear las migraciones
+    //     $migrate = fopen('C:\laragon\www/puntos_bat/php_fresh_'.$nombre.'.bat', 'a');
+    //     $texto_migrate='cd/
+        
+    //     cd laragon\www/74894537
+    //     php artisan migrate:fresh
+    //     php artisan migrate:fresh --seed';
+    //     fwrite($migrate,$texto_migrate);
+    //comando para importar en sql
+    // mysql -u root -e facturacion_74894537><C:\laragon\www\puntos_bat\data.sql ;
         // B-Correr el Archivo Bat
         // exec('c:\WINDOWS\system32\cmd.exe /c START C:\laragon\www/puntos_bat/php_fresh_'.$nombre.'.bat');
-        // $bat_migra='start /b  C:\laragon\www/puntos_bat/php_fresh_'.$nombre.'.bat';
-        // $btmi=pclose(popen($bat_migra, 'r'));
-
+         $bat_migra='start /b  C:\laragon\www/puntos_bat/php_fresh_74894537.bat';
+         $btmi=pclose(popen($bat_migra, 'r'));
+        return $bat_migra;
         // return json_encode(array('result'=>$btmi));
 
         // sleep(90);
